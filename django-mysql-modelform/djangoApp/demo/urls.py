9c557f4edb624f33d79391c3438b76d152ac35ed
purin-it <stanahas@amber.plala.or.jp>
2022-10-09 18:10:38 +0900

    django-mysql-modelform

--- django-mysql-modelform/djangoApp/demo/urls.py
+++ django-mysql-modelform/djangoApp/demo/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('confirm', views.confirm, name="confirm"),
-    path('regist', views.regist, name="regist"),
+    path('confirm', views.confirm, name='confirm'),
+    path('regist', views.regist, name='regist'),
 ]
